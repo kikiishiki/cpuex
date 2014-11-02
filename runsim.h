f4aaa38c79f9417b4cf98edf8b679c3b65b01c1b
kikiishiki <kikiishiki@gmail.com>
2014-11-03 02:03:57 +0900

    separate folders

--- runsim.h
+++
@@ -1,8 +0,0 @@
-#ifndef RUNSIM_H_
-#define RUNSIM_H_
-
-#include <stdint.h>
-
-void runsim(uint32_t code);
-
-#endif
--- util.h
+++
@@ -1,6 +0,0 @@
-#ifndef UTIL_H_
-#define UTIL_H_
-
-void error(char *fmt, ...);
-
-#endif
