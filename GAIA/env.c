c78b65cf5b252a776b91c09373047db45312112d
kikiishiki <kikiishiki@gmail.com>
2014-11-07 01:47:59 +0900

    2nd project GAIA has started

---
+++ GAIA/env.c
@@ -0,0 +1,38 @@
+#include "env.h"
+#include <stdio.h>
+#include <stdint.h>
+
+uint32_t ireg[IREG_NUM];
+uint32_t freg[FREG_NUM];
+uint32_t creg;
+uint32_t prog_cnt;
+uint32_t mem[MEM_SIZE];
+
+uint32_t max_stack = MEM_SIZE;
+uint32_t prog_size;
+
+void initialize_env() {
+  int i;
+  for (i = 0; i < IREG_NUM; ++i) ireg[i] = 0;
+  for (i = 0; i < FREG_NUM; ++i) freg[i] = 0;
+  ireg[STAC_PTR] = MEM_SIZE;
+  ireg[BASE_PTR] = MEM_SIZE;
+  prog_cnt = HEAP_ADDR + 1;
+  mem[HEAP_ADDR] = HEAP_ADDR + prog_size + 1;
+  
+
+}
+
+void print_env(FILE *fp) {
+  int i, bp;
+  for (i = 0; i < IREG_NUM; i++)
+    fprintf(fp, "$%2d = %11d (0x%08x)\n", i, ireg[i], ireg[i]);
+  fprintf(fp, "heap: %11d (0x%08x)\n", mem[HEAP_ADDR], mem[HEAP_ADDR]);
+  fprintf(fp, "inst: %11d (0x%08x)\n", mem[INST_PTR], mem[INST_PTR]);
+  fprintf(fp, "max stack: %d (0x%08x)\n", max_stack, max_stack);
+  fprintf(fp, "stack trace: %d", prog_cnt);
+  for (bp = ireg[BASE_PTR]; bp < MEM_SIZE; bp = mem[bp + 1])
+    fprintf(fp, " <- %d", mem[bp] + 3);
+  fprintf(fp, "\n");
+}
+
---
+++ GAIA/runsim.h
@@ -0,0 +1,8 @@
+#ifndef RUNSIM_H_
+#define RUNSIM_H_
+
+#include <stdint.h>
+
+void runsim(uint32_t code);
+
+#endif
